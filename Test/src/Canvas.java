d1c2fe384bb0ab5b09ec9a667b55cdd2ae5bafac
Jack <jack@jackv.co>
2016-05-11 08:57:01 -0700

    Added save() and load() to canvas

--- Test/src/Canvas.java
+++ Test/src/Canvas.java
@@ -1,6 +1,11 @@
 
 
 import java.awt.Graphics;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
@@ -15,12 +20,29 @@ public class Canvas extends JPanel {
 		shapes = new ArrayList<Shape>();
 	}
 	
-	public void save() {
-		// TODO
+	public boolean save(String fileName) {
+		try {
+			FileOutputStream fileOut = new FileOutputStream(fileName);
+			ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+			objOut.writeObject(shapes);
+			objOut.close();
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
 	}
 	
-	public void load() {
-		// TODO
+	@SuppressWarnings("unchecked")
+	public boolean load(String fileName) {
+		try {
+			FileInputStream fileIn = new FileInputStream(fileName);
+			ObjectInputStream objIn = new ObjectInputStream(fileIn);
+			shapes = (ArrayList<Shape>) objIn.readObject();
+			objIn.close();
+			return true;
+		} catch (IOException | ClassNotFoundException | ClassCastException e) {
+			return false;
+		}
 	}
 	
 	public void addShape(Shape shape) {
